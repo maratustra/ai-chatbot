a7de265fd54549967481ed63d2775b080bdec8aa
Alena Maramygina <57060511+maratustra@users.noreply.github.com>
2025-10-30 22:21:23 +0100

    setted structure to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
-import "./App.css"
-
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold text-blue-500">Hello World</h1>
-    </>
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-center py-8">AI Chatbot</h1>
+    </div>
   )
 }
 
